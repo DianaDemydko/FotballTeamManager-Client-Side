f9382df145b17e3379803414ac617c66458c0226
Diana Demydko <ddemy@softserveinc.com>
2020-12-24 16:05:30 +0200

    Added team name change logic

--- src/TeamPannel/TeamEditModal/index.js
+++ src/TeamPannel/TeamEditModal/index.js
@@ -13,14 +13,37 @@ import { isEmpty } from 'lodash';
 
 function TeamEditModal(props) {
     const {show, onHide, teamId, teamName} = props;
+    const [updatedTeamName, setTeamName] = useState(teamName);
     const { addToast } = useToasts();
     const [teamPlayersToChange, setTeamPlayers] = useState({});
     const saveChanges = () => {
+        if(teamName !== updatedTeamName && !isEmpty(updatedTeamName)) {
+            API.patch(`https://localhost:5001/api/Teams/ChangeTeamName/${teamId}/${updatedTeamName}`)
+            .then((response) => {
+                if(response.status === 200) {
+                    addToast(`Team name was updated`, {
+                        appearance: 'success',
+                        autoDismiss: true,
+                    });
+                }
+            })
+            .catch((error) => {
+                addToast(`Internal server error while updating team name: ${error}`, {
+                    appearance: 'error',
+                    autoDismiss: true,
+                });
+            });
+        } else {
+            addToast(`Team name cannot be empty`, {
+                appearance: 'warning',
+                autoDismiss: true,
+            });
+        }
         if (!isEmpty(teamPlayersToChange)) {
             teamPlayersToChange.map((player) => {
                 if(player.modifyed) {
-                        API.patch(`https://localhost:5001/api/Teams/ChangePlayerRole/${player.id}/${player.changedRoleId}`).then((responce) => {
-                        if(responce.status === 200) {
+                        API.patch(`https://localhost:5001/api/Teams/ChangePlayerRole/${player.id}/${player.changedRoleId}`).then((response) => {
+                        if(response.status === 200) {
                             addToast(`Player role was updated for ${player.name} ${player.surname}`, {
                                 appearance: 'success',
                                 autoDismiss: true,
@@ -36,6 +59,9 @@ function TeamEditModal(props) {
                 }
             });
         }
+    }
+    const teamNameChangehandler = (event) => {
+        setTeamName(event.target.value);
     }
     const changedPlayersInfo = (teamPlayers) => {
         setTeamPlayers(teamPlayers);
@@ -55,7 +81,7 @@ function TeamEditModal(props) {
                 <div className='edit-team-name-wrapper'>
                     <Form.Group controlId='edit-team-name'>
                         <Form.Label>Team Name:</Form.Label>
-                        <Form.Control defaultValue={teamName}/>
+                        <Form.Control defaultValue={updatedTeamName} onChange={teamNameChangehandler}/>
                     </Form.Group>
                 </div>
                 <div className='edit-team-members-wrapper'>
